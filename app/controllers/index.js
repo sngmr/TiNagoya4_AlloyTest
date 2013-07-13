f708e3df113d6f83f10ef116b518dd19e614b513
Shingo Mori <mori@connectionworks.jp>
2013-07-13 15:43:18 +0900

    ランダム表示に対応＆正解をクリックされたら色を変更

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,37 @@
-for (var i = 0; i < 25; i++) {
-	var ctl = Alloy.createController('touch', {number: i});
-	var touchView = ctl.getView();
-	$.touchArea.add(touchView);
-};
+// インスタンス変数
+var _numberObjects = {};
+var _nextNumber = 1;
 
+// 乱数で数字を作る
+// 結果はオブジェクトにControllerと共に入れる
+var count = 0;
+var min = 1;
+var max = 25
+while (count < 25) {
+	var numberCandidate = Math.floor(Math.random() * (max - min + 1)) + min;
+	if (!_.has(_numberObjects, numberCandidate)) {
+		_numberObjects[numberCandidate] = Alloy.createController('touch', {number: numberCandidate});
+		count += 1;
+	}
+}
+
+// 数字パネルを並べる
+for (var number in _numberObjects) {
+	_numberObjects[number].on('touch', touchHandler);
+	$.touchArea.add(_numberObjects[number].getView());
+}
+
+// Windowを開く
 $.window.open();
+
+// パネルクリック時ハンドラ
+function touchHandler(e) {
+	if (e.number == _nextNumber) {
+		// 正解
+		_numberObjects[e.number].correct();
+		_nextNumber += 1;
+	} else {
+		// 不正解
+		
+	}
+}
--- app/controllers/touch.js
+++ app/controllers/touch.js
@@ -1,3 +1,15 @@
 var args = arguments[0] || {};
 
+// 数字を画面に表示
 $.number.text = args.number;
+
+// タッチハンドラ
+$.touchArea.addEventListener('click', function(e) {
+	$.trigger('touch', {number: args.number});
+});
+
+// [PUBLIC]正解だから色を消す
+exports.correct = function() {
+	$.touchArea.backgroundColor = '#132A15';
+	$.number.color = '#888';
+}
